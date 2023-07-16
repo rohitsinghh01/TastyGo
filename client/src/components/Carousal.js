5804bcd5569c519d2c41cffcafb3cc03e8bd6982
Rohit <singh.rohitraj122@gmail.com>
2023-07-16 23:53:56 +0530

    carousal added

--- client/src/components/Carousal.js
+++ client/src/components/Carousal.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Carousal() {
   return (
     <>
-      <div class='carousal'>
-        <img src="images/briyani.jpg" alt="" className='pic'/>
-        <img src="images/briyani2.jpg" alt="" className='pic'/>
-        <img src="images/paneer.jpg" alt="" className='pic'/>
-        <img src="images/soup.jpg" alt="" className='pic'/>
+      <div className='carousal'>
+        <img src='images/briyani.jpg' alt='' />
+        <img src='images/briyani2.jpg' alt='' />
+        <img src='images/paneer.jpg' alt='' />
+        <img src='images/soup.jpg' alt='' />
+        <img src='images/noodles.jpg' alt='' />
+        <div className='text-overlay'>
+          <p className='title'>Are you Hungry?</p>
+          <p className='des'>
+            Explore a wide range of mouth-watering dishes and indulge in a
+            culinary journey that delights your taste buds.<br/>From aromatic
+            biryanis to delectable soups, we have something to satisfy every
+            craving.<br/> Enjoy the perfect blend of flavors and textures, crafted
+            with love and passion by our talented chefs.
+          </p>
+          <button className='btn'>
+            <Link to='' className='text'>
+              Order Now
+            </Link>
+          </button>
+        </div>
       </div>
     </>
   );
--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import Card from '../components/Card';
+// import Card from '../components/Card';
 import Carousal from '../components/Carousal';
 
 export default function Home() {
   return (
-    <div>
+    <div className='body'>
       <Navbar />
       <Carousal/>
-      <Card />
+      {/* <Card /> */}
       <Footer />
     </div>
   );
